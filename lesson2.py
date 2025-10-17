4986387e64f43b943a7b9579b528f7e5383ba1bd
Sandibell Vega <59941870+sanvega9@users.noreply.github.com>
2025-10-17 13:45:39 -0400

    Update lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -1,17 +1,26 @@
+
 import pandas as pd
-# Create a data with student data
+
+# Create a dataset with student data
 data = {'Name': ['Naomi', 'Jaden', 'Emmanuel'],
         'Age': [13, 15, 12],
         'Favorite_Subject': ['Science', 'Math', 'Art']}
+
 # Create a DataFrame
 df = pd.DataFrame(data)
+
 # Print the DataFrame
-print(df,"\n")
+print("Initial DataFrame:\n", df, "\n")
+
 # Access the 'Name' column
-print(df['Name'],"\n")
-# Add a new student 
+print("Names of Students:\n", df['Name'], "\n")
+
+# Add a new student
 df.loc[len(df)] = ['Andrew', 14, 'Science']
-print(df,"\n")
+df.loc[len(df)] = ['Jessica', 15, 'Science']
+print("After Adding a New Student:\n", df, "\n")
+
+
 # Add a new column 'Hobby'
-df['Hobby'] = ['Reading', 'Soccer', 'Gaming', 'Gaming']
-print(df,"\n")
+df['Hobby'] = ['Reading', 'Soccer', 'Gaming', 'Gaming','Art']
+print("After Adding the 'Hobby' Column:\n", df, "\n")
